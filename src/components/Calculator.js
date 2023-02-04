a441e123445ac89d4cbcbe0d5b6a29a253218db3
Anis BEN JEMIA <anis.ben-jemia@etu.univ-lyon1.fr>
2023-02-04 20:36:31 +0100

    Bug résolu, style très légèrement amélioré et calculette mise en place, le hook useReducer sera utilisé pour gérer la calculatrice, on peut appuyer sur chaque bouton de la calculette et la méthode pour gérer ce qu'il y a à gérer est appelée et fonctionne correctement selon le bouton sur lequel on a appuyé

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,30 +1,83 @@
 import "./Calculator.css";
+import {useReducer} from "react";
+
+
+const OPERATIONS = {
+    NEWNUMBER: "NewNumber",
+    ADD: "Add",
+    SUBSTRACT: "Substract",
+    MULTIPLY: "Multiply",
+    DIVIDE: "Divide",
+    RESET: "Reset",
+    DELETE: "Delete",
+    POINT: "Point",
+    EQUALS: "Equals"
+}
+
+
+
+function reducer(state, action) {
+    switch (action.type) {
+        case OPERATIONS.NEWNUMBER:
+            console.log("newNum -> "+action.newNumber);
+            break;
+        case OPERATIONS.ADD:
+            console.log("add");
+            break;
+        case OPERATIONS.SUBSTRACT:
+            console.log("sub");
+            break;
+        case OPERATIONS.MULTIPLY:
+            console.log("mul");
+            break;
+        case OPERATIONS.DIVIDE:
+            console.log("divid");
+            break;
+        case OPERATIONS.RESET:
+            console.log("reset");
+            break;
+        case OPERATIONS.DELETE:
+            console.log("delete");
+            break;
+        case OPERATIONS.POINT:
+            console.log("point");
+            break;
+        case OPERATIONS.EQUALS:
+            console.log("equals");
+            break;
+        default:
+            console.log("ERREUR REDUCER");
+    }
+}
 
 
 function Calculator() {
+    const [state, dispatch] = useReducer(reducer, {});
+    
     return (
         <div className="calculator-grid">
             <div className="output">
-                OUTPUT
+                <div className="calculation">CALCULATION</div>
+                <div className="result">RESULT</div>
             </div>
-            <button className="span-two">AC</button>
-            <button>DEL</button>
-            <button>÷</button>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>x</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>+</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>-</button>
-            <button>.</button>
-            <button>0</button>
-            <button className="span-two">=</button>
+            <button className="span-two" onClick={() => dispatch({type: "Reset"})}>AC</button>
+            <button onClick={() => dispatch({type: "Delete"})}>DEL</button>
+            <button onClick={() => dispatch({type: "Divide"})}>÷</button>
+            <button onClick={() => dispatch({type: "NewNumber", newNumber: 1})}>1</button>
+            <button onClick={() => dispatch({type: "NewNumber", newNumber: 2})}>2</button>
+            <button onClick={() => dispatch({type: "NewNumber", newNumber: 3})}>3</button>
+            <button onClick={() => dispatch({type: "Multiply"})}>x</button>
+            <button onClick={() => dispatch({type: "NewNumber", newNumber: 4})}>4</button>
+            <button onClick={() => dispatch({type: "NewNumber", newNumber: 5})}>5</button>
+            <button onClick={() => dispatch({type: "NewNumber", newNumber: 6})}>6</button>
+            <button onClick={() => dispatch({type: "Add"})}>+</button>
+            <button onClick={() => dispatch({type: "NewNumber", newNumber: 7})}>7</button>
+            <button onClick={() => dispatch({type: "NewNumber", newNumber: 8})}>8</button>
+            <button onClick={() => dispatch({type: "NewNumber", newNumber: 9})}>9</button>
+            <button onClick={() => dispatch({type: "Substract"})}>-</button>
+            <button onClick={() => dispatch({type: "Point"})}>.</button>
+            <button onClick={() => dispatch({type: "NewNumber", newNumber: 0})}>0</button>
+            <button className="span-two" onClick={() => dispatch({type: "Equals"})}>=</button>
         </div>
     );
 }
--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -2,7 +2,15 @@ import './Presentation.css';
 
 function Presentation() {
     return (
-        <div className="presentation">Text</div>
+        <div className="presentation">
+            Bienvenue sur ce site développé par Anis BEN JEMIA (étudiant en 2ème année de BUT Informatique).
+            Le but de ce site est de faire un projet d'introduction au React pour apprendre les concepts principaux
+            du framework. Pour l'instant tout n'est pas opérationnel mais je suis plutôt ambiteux: je ferais en sorte
+            de créer une Interface Utilisateur Dynamique et d'ajouter des animations, de mieux styliser le site et
+            peut-être de proposer différents types de calculatrices au fur et à mesure que j'avance dans ce projet;
+            le but pour l'instant étant de se familiariser avec ReactJS et ses aspects techniques et non pas de faire
+            du CSS ou de réaliser un site agréable à regarder sans but derrière
+        </div>
     );
 }
 
