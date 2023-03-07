7ab186271768aa2433bb0220ab83b3046b90f674
Anis BEN JEMIA <anis.ben-jemia@etu.univ-lyon1.fr>
2023-03-07 21:47:21 +0100

    Mise en place d'animations au chargement de la page et à leur disparation pour les divs de descriptions (avec framer motion), changement de la couleur de la calculatrice, descriptions mises à jour, ajout du logo Reacy

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,10 +1,15 @@
 import './AppHeader.css';
-
+import logo from "../logo.svg";
 
 function AppHeader() {
     return (
         <header className="App-header">
-            <h1 className="header-title">ReactJS Calculator</h1>
+            <h1 className="header-title">
+                <img src={logo} className="react-logo-header"/>
+                ReactJS Calculator
+                <img src={logo} className="react-logo-header"/>
+            </h1>
+            <img src={logo}/>
         </header>
     );
 }
--- src/components/CalcDescription.jsx
+++ src/components/CalcDescription.jsx
@@ -1,5 +1,6 @@
-import React, {useEffect, useLayoutEffect, useState} from "react";
-import {motion} from "framer-motion";
+import React, {useLayoutEffect, useState} from "react";
+import {AnimatePresence, motion} from "framer-motion";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./CalcDescription.css";
 
 function CalcDescription() {
@@ -25,25 +26,61 @@ function CalcDescription() {
     }, [respawn])
     
     return (
-        <div as={motion.div} className="calc-descriptions-container d-flex flex-row justify-content-evenly my-5">
+        <div className="container-fluid d-flex flex-row justify-content-evenly position-relative">
             {showRespawn &&
-                <button className="respawn-button text-light align-self-center text-wrap p-1" onClick={() => setRespawn(true)}>Remontrer Descriptions</button>
+                <button className="respawn-button text-light text-wrap p-1 align-self-center position-absolute" onClick={() => setRespawn(true)}>Faire Réapparaître Descriptions</button>
             }
             
-            {showFirst &&
-            <div component={motion.div}
-                 
-                 className="calc-description-div bg-dark p-3 d-flex flex-column justify-content-between">
-                <div className="calc-description-text text-light text-break fs-5">Pour gérer les résultats affichés par la calculatrice, le hook <strong>useReducer</strong> a été utilisé et est mis à jour à chaque fois qu'on appuie sur un bouton de la calculatrice</div>
-                *<button className="calc-description-button align-self-center text-light" onClick={() => setShowFirst(!showFirst)}>CACHER</button>
+            <div className="calc-descriptions-container d-flex flex-row justify-content-evenly my-5 ">
+                <AnimatePresence>
+                    {showFirst && (
+                        <motion.div
+                             initial={{
+                                 x: "-2vw",
+                                 y: "10vh"
+                            }}
+                             animate={{
+                                 x: 0,
+                                 y: 0
+                             }}
+                             transition={{
+                                 duration: 0.8
+                             }}
+                             exit={{
+                                 opacity: 0,
+                             }}
+                             className="calc-description-div bg-dark p-3 d-flex flex-column justify-content-between"
+                             >
+                            <div className="calc-description-text text-light text-break fs-5">Pour gérer les résultats affichés par la calculatrice, le hook <strong>useReducer</strong> a été utilisé et est mis à jour à chaque fois qu'on appuie sur un bouton de la calculatrice</div>
+                            <button className="calc-description-button align-self-center my-2 text-light" onClick={() => setShowFirst(!showFirst)}>CACHER</button>
+                        </motion.div>
+                    )}
+                </AnimatePresence>
+                
+                <AnimatePresence>
+                    {showSecond && (
+                        <motion.div
+                            initial={{
+                                x: "2vw",
+                                y: "10vh"
+                            }}
+                            animate={{
+                                x: 0,
+                                y: 0,
+                            }}
+                            transition={{
+                                duration: 0.8
+                            }}
+                            exit={{
+                                opacity: 0,
+                            }}
+                            className="calc-description-div bg-dark p-3 d-flex flex-column justify-content-between">
+                            <div className="calc-description-text text-light text-break fs-5">Pour faire disparraître et réapparaître ces div et le bouton de réapparition, les hooks <strong>useState</strong> et <strong>useLayoutEffect</strong> ont été utilisés et sont mis à jour au click sur un des boutons </div>
+                            <button className="calc-description-button align-self-center my-2 text-light" onClick={() => setShowSecond(!showSecond)}>CACHER</button>
+                        </motion.div>
+                    )}
+                </AnimatePresence>
             </div>
-            }
-            {showSecond &&
-            <div as={motion.div} className="calc-description-div bg-dark p-3 d-flex flex-column justify-content-between">
-                <div className="calc-description-text text-light text-break fs-5">Pour gérer les résultats affichés par la calculatrice, le hook <strong>useReducer</strong> a été utilisé et est mis à jour à chaque fois qu'on appuie sur un bouton de la calculatrice</div>
-                <button className="calc-description-button align-self-center text-light" onClick={() => setShowSecond(!showSecond)}>CACHER</button>
-            </div>
-            }
         </div>
     )
 }
